e7660ac8b9a87e1b4fd75d7a23593a81fc1cd8fb
Bart Kooijmans <bart.kooijmans@zwijsen.nl>
2024-04-24 10:45:48 +0200

    remove Ilogging

--- WikiArtParser.Core/WikiArtParserMessageHandler.cs
+++ WikiArtParser.Core/WikiArtParserMessageHandler.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.Extensions.Logging;
-using System.Text.Json;
-using WikiArtParser.Core.Interfaces;
+﻿using WikiArtParser.Core.Interfaces;
 using WikiArtParser.Core.Models;
 
 namespace WikiArtParser.Core
@@ -9,19 +7,16 @@ namespace WikiArtParser.Core
     {
         private readonly IWikiArtParser _wikiArtParser;
         private readonly IArtRepository _artRepository;
-        private readonly ILogger<WikiArtParserMessageHandler> _logger;
 
-        public WikiArtParserMessageHandler(IWikiArtParser wikiArtParser, IArtRepository artRepository, ILogger<WikiArtParserMessageHandler> logger)
+        public WikiArtParserMessageHandler(IWikiArtParser wikiArtParser, IArtRepository artRepository)
         {
             _wikiArtParser = wikiArtParser;
             _artRepository = artRepository;
-            _logger = logger;
         }
 
         public async Task Handle(WikiArtParserMessage message)
         {
             var artwork = await _wikiArtParser.ParseUrl(message.Url);
-            _logger.LogInformation(JsonSerializer.Serialize(artwork));
 
             await _artRepository.SaveAsync(artwork);
         }
